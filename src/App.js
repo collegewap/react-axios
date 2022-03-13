56850ff4c39eab409ca852e1dc7869030b60fcb9
Abhishek EH <collegewap@gmail.com>
2022-03-13 19:21:14 +0530

    update data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import { Card } from "@blueprintjs/core";
+import {
+  Card,
+  Button,
+  EditableText,
+  Toaster,
+  Position,
+} from "@blueprintjs/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
+
+const AppToaster = Toaster.create({
+  position: Position.TOP,
+});
 
 function App() {
   const [persons, setPersons] = useState([]);
@@ -10,6 +20,25 @@ function App() {
     });
   }, []);
 
+  const onChangeHandler = (id, key, value) => {
+    setPersons((values) => {
+      return values.map((item) =>
+        item.id === id ? { ...item, [key]: value } : item
+      );
+    });
+  };
+
+  const updateData = (id) => {
+    const data = persons.find((item) => item.id === id);
+    axios.put(`http://localhost:8001/persons/${id}`, data).then((response) => {
+      AppToaster.show({
+        message: "Data updated successfully",
+        intent: "success",
+        timeout: 3000,
+      });
+    });
+  };
+
   return (
     <div className="App">
       <Card>
@@ -18,14 +47,37 @@ function App() {
             <tr>
               <th>Name</th>
               <th>Job Title</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             {persons.map((person) => {
               return (
                 <tr key={person.id}>
-                  <td>{person.name}</td>
-                  <td>{person.jobTitle}</td>
+                  <td>
+                    <EditableText
+                      value={person.name}
+                      onChange={(value) =>
+                        onChangeHandler(person.id, "name", value)
+                      }
+                    />
+                  </td>
+                  <td>
+                    <EditableText
+                      value={person.jobTitle}
+                      onChange={(value) =>
+                        onChangeHandler(person.id, "jobTitle", value)
+                      }
+                    />
+                  </td>
+                  <td>
+                    <Button
+                      intent="primary"
+                      onClick={() => updateData(person.id)}
+                    >
+                      Update
+                    </Button>
+                  </td>
                 </tr>
               );
             })}
